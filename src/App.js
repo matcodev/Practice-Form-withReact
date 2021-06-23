f44622ceaf123745c7a9a9c20833672e30c76430
mxespin <matias@clippercapital.cl>
2021-06-23 12:02:11 -0400

    Personalización, estilos y barra nav con favoritos

--- src/App.js
+++ src/App.js
@@ -1,77 +1,135 @@
-import React, {useState} from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
-  const db = [{
-    id: 1,
-    nombre: "María",
-    apellido: "Castillo"
-  },
-  {
-    id: 2,
-    nombre: "José",
-    apellido: "Caro"
-  }
+  const db = [
+    {
+      id: 1,
+      personaje: 'Capitán América',
+      pelicula: 'El primer Vengador'
+    },
+    {
+      id: 2,
+      personaje: 'Iron Man',
+      pelicula: 'Avengers: Infinity Wars'
+    }
   ];
 
   const [datos, setDatos] = useState(db);
 
   const [form, setForm] = useState({
     id: null,
-    nombre: "",
-    apellido: ""
-  })
+    personaje: '',
+    pelicula: ''
+  });
 
-  const capturadorInput = (e) => {
-    const {name, value} = e.target;
-     // console.log(value)
+  const capturadorInput = e => {
+    const { name, value } = e.target;
+    // console.log(value)
     setForm({
       ...form,
-      [name] : value
-    })
-  }
-  const crearDatos = (nuevoIngreso) => {
-    const id = datos[datos.length-1].id + 1;
-    const nuevosDatos = {...nuevoIngreso, id: id}
+      [name]: value
+    });
+  };
+  const crearDatos = nuevoIngreso => {
+    const id = datos[datos.length - 1].id + 1;
+    const nuevosDatos = { ...nuevoIngreso, id: id };
     //console.log(nuevosDatos)
-    setDatos([
-      ...datos,
-      nuevosDatos
-    ])
-  }
-  const enviarFormulario = (e) => {
-      e.preventDefault();
-      crearDatos(form)
-      setForm()
-  }
+    setDatos([...datos, nuevosDatos]);
+  };
+  const enviarFormulario = e => {
+    e.preventDefault();
+    crearDatos(form);
+    e.target.reset();
+  };
+
+  const [favoritos, setFavoritos] = useState([{ id: '', personaje: '' }]);
 
   return (
-    <div>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Personajes Favoritos
+          </a>
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton2"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Favoritos <i className="fas fa-star text-warning" />
+            </button>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="dropdownMenuButton2"
+            >
+              {favoritos.map(favorito => {
+                return (
+                  <li className="dropdown-item px-3">
+                    {favorito.personaje}
+                    <button className="btn btn-danger ms-5">
+                      <i className="fas fa-trash" />
+                    </button>
+                  </li>
+                );
+              })}
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li className="d-grid gap-2">
+                <button className="dropdown-item">Eliminar todos</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <form onSubmit={enviarFormulario}>
-        <input 
-          name="nombre"
+        <input
+          className="form-control my-2"
+          name="personaje"
           type="text"
-          placeholder="nombre"
+          placeholder="Nombre Personaje"
           onChange={capturadorInput}
         />
-        <input 
-          name="apellido"
+        <input
+          className="form-control my-2"
+          name="pelicula"
           type="text"
-          placeholder="apellido"
+          placeholder="Pelicula"
           onChange={capturadorInput}
         />
-        <button type="submit">Enviar</button>
+        <button className="btn btn-primary" type="submit">
+          Crear
+        </button>
       </form>
-      <hr/>
-      <ul>
-        {
-          datos.map(item => {
+      <hr />
+      <div className="container">
+        <p className="text-center">
+          Haz click en el <i className="fas fa-heart text-danger" /> para añadir
+          a favoritos
+        </p>
+        <div className="row">
+          {datos.map(item => {
             return (
-              <li key={item.id}>{item.id}- {item.nombre} {item.apellido} </li>
-            )
-          })
-        }
-      </ul>
+              <div key={item.id} className="col-6 my-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.personaje}</h5>
+                    <p className="card-text">{item.pelicula}</p>
+                    <a href="#" className="btn btn-light">
+                      <i className="fas fa-heart text-danger" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
